8d4c9a967b4b27757a47201e0ece8157c27d438f
Anirudh Naginayani Cheruvu <anirudh05nc@gmail.com>
2025-06-17 19:26:26 +0530

    Changes in Settings.py

--- Taskmanager/settings.py
+++ Taskmanager/settings.py
@@ -144,10 +144,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 import os
 
 STATIC_URL = '/static/'
+
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
--- Taskmanager/wsgi.py
+++ Taskmanager/wsgi.py
@@ -7,14 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
 
+
+import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Taskmanager.settings")
 
 application = get_wsgi_application()
-
-
-from whitenoise import WhiteNoise
 application = WhiteNoise(application)
